c6cc2233633b8a8092a0e84e7bf099d4ffe27057
Dinni Manish <manishdinni.0935@gmail.com>
2026-08-29 23:33:41 +0530

    ✅ Min and Max in Array | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-29

---
+++ gfg/basic/find-minimum-and-maximum-element-in-an-array4428/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int min = arr[0];
+        int max = arr[0];
+        
+        for(int i =0 ;i<arr.length;i++){
+            
+            if(arr[i] < min){
+                min = arr[i];
+                
+            }
+            
+            if(arr[i] > max){
+                max = arr[i];
+                
+            }
+            
+        }
+        
+        list.add(min);
+        list.add(max);
+        
+        return list;
+    }
+}
