aede20e8414efa5c1946dddf9cb2423b52e0f36a
Dinni Manish <manishdinni.0935@gmail.com>
2026-09-12 20:19:14 +0530

    ✅ Reverse Array | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-12

---
+++ gfg/easy/reverse-an-array/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        
+        
+        int temp;
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        
+        
+        
+    }
+}
