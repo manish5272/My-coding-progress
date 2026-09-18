9aec034a5a6a66dbb6704ef5bd15b11b2f48312c
Dinni Manish <manishdinni.0935@gmail.com>
2026-09-18 18:22:39 +0530

    ✅ Length of Last Word | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.2 MB | 2026-09-18

---
+++ leetcode/easy/0058-length-of-last-word/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String s1 = s.trim();
+
+
+        int count = 0;
+        for(int i = s1.length()-1 ;i>=0;i--){
+            if(s1.charAt(i) != ' ')
+            {
+                count++;
+                
+            }
+
+            else if(s1.charAt(i) == ' '){
+                break;
+            }
+        }
+
+        return count;
+    }
+}
