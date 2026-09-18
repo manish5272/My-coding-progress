95fee540438e149dffc4f17aef7e7b1091bd0c1e
Dinni Manish <manishdinni.0935@gmail.com>
2026-09-19 02:09:58 +0530

    ✅ Number of Segments in a String | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-09-18

--- leetcode/easy/0434-number-of-segments-in-a-string/solution.java
+++ leetcode/easy/0434-number-of-segments-in-a-string/solution.java
@@ -6,12 +6,12 @@ class Solution {
 
         for(int i =0 ;i<s.length()-1;i++){
             
-            if((s.length()-1)==0){
+            if(sorted.length()==0){
                 count=0;
                 break;
             }
             
-            else if(s.charAt(i) == ' '){
+            else if(sorted.charAt(i) == ' '){
                 count++; 
             }
             
