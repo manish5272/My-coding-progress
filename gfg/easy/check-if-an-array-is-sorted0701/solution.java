fc742d0b3bdd80e33207163ea7c487ced94b9059
Dinni Manish <manishdinni.0935@gmail.com>
2026-09-18 18:44:56 +0530

    ✅ Check Sorted Array | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-18

---
+++ gfg/easy/check-if-an-array-is-sorted0701/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        
+        boolean isSorted = true;
+        for(int i= 0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted = false;
+                break;
+                
+            }
+        }
+        return isSorted;
+        
+        
+    }
+}
