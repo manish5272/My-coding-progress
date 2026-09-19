6ccb0df4011db03885267021cf48b4eb4f8e258d
Dinni Manish <manishdinni.0935@gmail.com>
2026-09-19 22:23:37 +0530

    ✅ Convert String to Lower Case | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ gfg/basic/convert-string-to-lowercase/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    static String toLower(String s) {
+        
+        
+        String modified=s.toLowerCase();
+        
+        return modified;
+        
+        
+        
+        
+    }
+}
